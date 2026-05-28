82c39a3442fb664fac4f5070165453931d459074
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-05-28 11:48:39 +1000

    feat(ecs): Add VCF validation Docker image assets

---
+++ app/ecs/validate_vcf_file/docker-entrypoint.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Get the presigned url
+set -euo pipefail
+
+# Check the following env vars are set
+if [[ ! -v HOSTNAME_SSM_PARAMETER_NAME ]]; then
+  echo "Error: HOSTNAME_SSM_PARAMETER_NAME is not set" 1>&2
+  exit 1
+fi
+if [[ ! -v ORCABUS_TOKEN_SECRET_ID ]]; then
+  echo "Error: ORCABUS_TOKEN_SECRET_ID is not set" 1>&2
+  exit 1
+fi
+if [[ ! -v INPUT_VCF_URI ]]; then
+  echo "Error: INPUT_VCF_URI is not set" 1>&2
+  exit 1
+fi
+
+# Set the HOSTNAME environment variable
+HOSTNAME="$( \
+  aws ssm get-parameter \
+	--name "${HOSTNAME_SSM_PARAMETER_NAME}" \
+	--output json | \
+  jq --raw-output '.Parameter.Value' \
+)"
+export HOSTNAME
+
+# Set ORCABUS_TOKEN environment variable
+ORCABUS_TOKEN="$( \
+  aws secretsmanager get-secret-value \
+	--secret-id "${ORCABUS_TOKEN_SECRET_ID}" \
+	--output json \
+	--query SecretString | \
+  jq --raw-output 'fromjson | .id_token' \
+)"
+export ORCABUS_TOKEN
+
+# Get filemanager parameter inputs
+bucket="$(
+	python3 -c "from urllib.parse import urlparse; print(urlparse('${INPUT_VCF_URI}').netloc)"
+)"
+key="$(
+	python3 -c "from urllib.parse import urlparse; print(urlparse('${INPUT_VCF_URI}').path.lstrip('/'))"
+)"
+
+download_file_from_filemanager(){
+  : '
+  Download a file from the filemanager
+  '
+  # Inputs
+  local bucket="${1}"
+  local key="${2}"
+
+  # Input vars
+  local CURL_GET_S3_PRESIGNED_URL_ARGS_ARRAY
+
+  # Get vcf presigned url
+  CURL_GET_S3_PRESIGNED_URL_ARGS_ARRAY=( \
+    "--fail" "--silent" "--location" "--show-error" \
+    "--header" "Accept: application/json" \
+    "--header" "Authorization: Bearer ${ORCABUS_TOKEN}" \
+    "https://file.${HOSTNAME}/api/v1/s3/presign?&responseContentDisposition=inline&bucket=${bucket}&key=${key}" \
+  )
+
+  # Download the file
+  wget \
+    --output-document "$(basename "${key}")" \
+    "$( \
+      curl "${CURL_GET_S3_PRESIGNED_URL_ARGS_ARRAY[@]}" | \
+      jq --raw-output '.results[0]' \
+    )"
+
+}
+
+# Download the files from the filemanager
+download_file_from_filemanager "${bucket}" "${key}"
+download_file_from_filemanager "${bucket}" "${key}.tbi"
+
+# Try and view the vcf file from vcf tools
+bcftools view "$(basename "${key}")" 1>/dev/null 2>error_logs.txt
+
+if [[ -s "error_logs.txt" ]]; then
+  echo "Error viewing the vcf s3://${bucket}/${key}"
+  exit 1
+fi
